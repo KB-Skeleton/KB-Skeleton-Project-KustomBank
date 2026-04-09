4e10ce4e155d41fba04612d9105c4f0d15ebec25
hyeongls <lshyeong47@naver.com>
2026-04-09 09:47:37 +0900

    feat: 투자 페이지 뼈대 개발

--- src/views/InvestmentView.vue
+++ src/views/InvestmentView.vue
@@ -1,2 +1,19 @@
-<template></template>
-<script setup></script>
+<template>
+  <!--불필요한 투자 지출-->
+  <section class="d-grid gap-3">
+    <InvestSummary> </InvestSummary>
+  </section>
+
+  <article class="row g-3">
+    <!--인기 종목 리스트-->
+    <HotStockList></HotStockList>
+
+    <!--투자 아이디어 & 상식-->
+    <InvestmentTips></InvestmentTips>
+  </article>
+</template>
+<script setup>
+import HotStockList from "@/components/investment/HotStockList.vue";
+import InvestmentTips from "@/components/investment/InvestmentTips.vue";
+import InvestSummary from "@/components/investment/InvestSummary.vue";
+</script>
