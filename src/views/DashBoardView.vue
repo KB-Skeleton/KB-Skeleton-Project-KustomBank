b1fc089ed74787eff74245feb7e983daeb18827b
hyeongls <lshyeong47@naver.com>
2026-04-10 19:42:40 +0900

    feat: 대시보드 홈 골자 개발 - #21

--- src/views/DashBoardView.vue
+++ src/views/DashBoardView.vue
@@ -1,2 +1,39 @@
-<template></template>
-<script></script>
+<template>
+  <!--대시보드 서머리-->
+  <section class="d-gird gap-3">
+    <SummaryCards />
+
+    <!--예산 소진 바-->
+    <article class="kb-panel kb-clickable" @click="router.push('/budget')">
+      <BudgetProgress></BudgetProgress>
+    </article>
+
+    <!--소비 도넛 차트-->
+    <div class="row g-3">
+      <div class="col-12 col-xl-6">
+        <article
+          class="kb-panel kb-clickable"
+          @click="router.push('/statistics')"
+        >
+          <ExpenseDonutChart></ExpenseDonutChart>
+        </article>
+      </div>
+    </div>
+
+    <!--최근 소비 내역-->
+    <div class="col-12 col-xl-6">
+      <article class="kb-panel">
+        <RecentTransactions />
+      </article>
+    </div>
+  </section>
+</template>
+<script setup>
+import { useRouter } from "vue-router";
+import BudgetProgress from "@/components/dashBoard/BudgetProgress.vue";
+import SummaryCards from "@/components/dashBoard/SummaryCards.vue";
+import ExpenseDonutChart from "@/components/dashBoard/ExpenseDonutChart.vue";
+import RecentTransactions from "@/components/dashBoard/RecentTransactions.vue";
+
+const router = useRouter();
+</script>
